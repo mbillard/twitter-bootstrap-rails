427d032de44af4c33ef63942256185d7ee552963
Michel Billard <michel@mbillard.com>
2013-06-03 15:29:05 -0400

    render_breadcrumbs now accepts a &block argument

--- app/helpers/twitter_breadcrumbs_helper.rb
+++ app/helpers/twitter_breadcrumbs_helper.rb
@@ -1,5 +1,10 @@
 module TwitterBreadcrumbsHelper
   def render_breadcrumbs(divider = '/')
-    render :partial => 'twitter-bootstrap/breadcrumbs', :locals => { :divider => divider }
+    content = render :partial => 'twitter-bootstrap/breadcrumbs', :layout => false, :locals => { :divider => divider }
+    if content.present? and block_given?
+      capture(content, &block)
+    else
+      content
+    end
   end
 end
